0d740bba90550f39c79459f01bba56f145a20e2d
Rafael Fernandes <81439181+Rafaeli9@users.noreply.github.com>
2021-06-10 16:28:02 -0300

    Add files via upload

---
+++ soma/divisao.js
@@ -0,0 +1,84 @@
+const aleatorio = (min, max) =>
+Math.floor(Math.random() * (max - min + 1)) + min
+
+var numero1 = parseInt(aleatorio(1, 10));
+var numero2 = parseInt(aleatorio(1, 10));
+var soma = parseInt(numero1 / numero2);
+var resultado = "Qual a divisão de: " + numero1 + " / " + numero2 + " ?";
+let btnChange = document.querySelector('#checar');
+let btnChange2 = document.querySelector('#startOne');
+let btnChangeOne = document.querySelector('#startTwo');
+
+function changeDivi(){
+    document.getElementById("mudar").innerHTML = (resultado);    
+}
+
+function atualizaDivi(){
+    window.location.reload();
+}
+
+function mudaBtn(){
+    btnChange2.addEventListener('click', () => 
+    btnChangeOne.style.backgroundColor='#ee1d52',
+    btnChange.style.backgroundColor='#42f548',
+    btnChange2.style.backgroundColor='#ee1d52')  
+}
+
+function mudaBtn1(){
+    btnChange.addEventListener('click', () =>      
+    btnChange2.style.backgroundColor='#ee1d52',
+    btnChange.style.backgroundColor='#ee1d52', 
+    btnChangeOne.style.backgroundColor='#42f548',
+    )  
+}
+         
+
+function conferirDivi() {  
+          
+    var conf = document.querySelector(".confere").value;
+    if (conf == soma){        
+        Swal.fire({
+            title: 'Parabéns!!!',
+            text: 'Você acertou!!!',
+            imageUrl: 'image/sim.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: 'image/sim.jpg',
+            backdrop: ` rgba(255,255,0,0.4)
+                        url("image/acerto.png")
+                        center top
+                        1.35rem
+                        no-repeat`
+          })
+        //   btnChange.addEventListener('click', () => btnChange.style.backgroundColor='red',
+        //   btnChange2.style.backgroundColor='#ee1d52',
+        //   btnChangeOne.style.backgroundColor='green')       
+    }else if (conf == "") {            
+        Swal.fire({
+            title: 'ERROOO!!!',
+            text: 'Favor digitar sua resposta!!!',
+            imageUrl: 'image/nao.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: 'image/nao.jpg',
+            backdrop: ``
+        }) }else { 
+        Swal.fire({
+            title: 'Que triste!!!',
+            text: 'Você errou!!!',
+            imageUrl: 'image/nao.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: 'image/nao.jpg',
+            backdrop: ` rgba(0,0,255,0.4)
+                        url("image/erro.png")
+                        center top
+                        2rem
+                        no-repeat`
+          })
+        // btnChange.addEventListener('click', () => btnChange.style.backgroundColor='red', 
+        // btnChange2.style.backgroundColor='#ee1d52',
+        // btnChangeOne.style.backgroundColor='green')
+        
+    };
+}
---
+++ soma/soma.js
@@ -0,0 +1,78 @@
+
+const aleatorio = (min, max) =>
+Math.floor(Math.random() * (max - min + 1)) + min
+
+var numero1 = parseInt(aleatorio(1, 10));
+var numero2 = parseInt(aleatorio(1, 10));
+var soma = parseInt(numero1 + numero2);
+var resultado = "Qual a soma de:  " + numero1 + " + " + numero2 + " ?";
+let btnChange = document.querySelector('#checar');
+let btnChange2 = document.querySelector('#startOne');
+let btnChangeOne = document.querySelector('#startTwo');
+
+function changeSoma(){
+    document.getElementById("mudar").innerHTML = (resultado);    
+}
+
+function atualizaSoma(){
+    window.location.reload();
+}
+
+function mudaBtn(){
+    btnChange2.addEventListener('click', () => 
+    btnChangeOne.style.backgroundColor='#ee1d52',
+    btnChange.style.backgroundColor='#42f548',
+    btnChange2.style.backgroundColor='#ee1d52')  
+}
+
+function mudaBtn1(){
+    btnChange.addEventListener('click', () =>      
+    btnChange2.style.backgroundColor='#ee1d52',
+    btnChange.style.backgroundColor='#ee1d52', 
+    btnChangeOne.style.backgroundColor='#42f548',
+    )  
+}
+         
+
+function conferirSoma() {  
+          
+    var conf = document.querySelector(".confere").value;
+    if (conf == soma){        
+        Swal.fire({
+            title: 'Parabéns!!!',
+            text: 'Você acertou!!!',
+            imageUrl: 'image/sim.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: 'image/sim.jpg',
+            backdrop: ` rgba(255,255,0,0.4)
+                        url("image/acerto.png")
+                        center top
+                        1.35rem
+                        no-repeat`
+          })    
+    }else if (conf == "") {            
+        Swal.fire({
+            title: 'ERROOO!!!',
+            text: 'Favor digitar sua resposta!!!',
+            imageUrl: 'image/nao.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: 'image/nao.jpg',
+            backdrop: ``
+        }) }else { 
+        Swal.fire({
+            title: 'Que triste!!!',
+            text: 'Você errou!!!',
+            imageUrl: 'image/nao.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: 'image/nao.jpg',
+            backdrop: ` rgba(0,0,255,0.4)
+                        url("image/erro.png")
+                        center top
+                        2rem
+                        no-repeat`
+          })
+    };
+}
